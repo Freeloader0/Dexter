a4ead2b25a6938f20f39ffc938cb8774778cd4c1
Freeloader0 <stephenlewis0@gmail.com>
2015-12-30 16:33:20 -0500

    writeLog
    Added writeLog function and fixed .gitignore filex

--- dexter.py
+++ dexter.py
@@ -1,11 +1,42 @@
 # dexter.py
-# Main portion of the Dexter project
+# Main module of the Dexter project
 #
 
+# Imports
+import datetime
+import os.path
+
+
+# Local Dexter imports
 from getHostInfo import *
 
+
+# Global variables
+environ = getHostInfo()
+
+
+# Function to write a string into the Dexter Log
+def writeLog(message):
+    message = str(datetime.datetime.now()) + ': ' + message + '\r\n'
+    
+    # Assert that the log file has been designated
+    if 'DEXTERLOG' not in environ.keys():
+        environ['DEXTERLOG'] = str(os.path.join(environ['PUBLIC'], 'dexter.log'))
+       
+    # Attempt to write to log file
+    try:
+        with open(environ['DEXTERLOG'], 'a') as f:
+            f.write(message)
+    except:
+        return -1
+        
+    return 0
+
+    
+# Main subroutine    
 def main():
-    print(getHostInfo())
+    writeLog('Dexter Started')
+    
     pass
 
 if __name__ == '__main__':

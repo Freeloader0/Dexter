09d93ff5f15d0336f0d2f0c942f8390d820b5ab5
Freeloader0 <stephenlewis0@gmail.com>
2016-01-04 08:26:06 -0500

    Fixed IP Address grepping in getHostInfo
    Windows XP only has "IP Address", not "IPv4" or "IPv6"

--- getHostInfo.py
+++ getHostInfo.py
@@ -35,7 +35,7 @@ def getHostInfo():
     for l in lines:
         if str.find(l, 'Physical Address') > -1:
             info['MACADDRESS'].append(str.split(l)[-1])
-        elif str.find(l, 'IPv4 Address') > -1 or str.find(l, 'IPv6 Address') > -1:
+        elif str.find(l, 'IPv4 Address.') > -1 or str.find(l, 'IPv6 Address.') > -1 or str.find(l, 'IP Address.') > -1:
             info['IPADDRESS'].append(str.split(l)[-1].replace('(Preferred)', ''))
         
     return info

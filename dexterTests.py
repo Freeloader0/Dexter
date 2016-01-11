85f4e8a18cc7ea8cca3cd6de3a6b0a942d9de897
Freeloader0 <stephenlewis0@gmail.com>
2016-01-11 10:28:50 -0500

    Added unit testing
    Added dexterTests.py for unittesting.  Modified runSystemCommand.py to
    suppress STDERR messages.  Added __init__.py in an attempt at packaging.

---
+++ dexterTests.py
@@ -0,0 +1,47 @@
+# dexterTests.py
+# Unit testing for project Dexter
+#
+# Last updated: 1/11/2016
+# Functions supported:
+### runSystemCommand
+### getHostInfo
+### dexter.writeLog
+# Functions TODO:
+### None :)
+
+import unittest
+import os.path
+
+# Dexter specific imports
+from dexter import *
+from getHostInfo import *
+from runSystemCommand import *
+from subprocess import CalledProcessError
+
+
+class testRunSystemCommand(unittest.TestCase):
+
+    def testRunSystemCommand_correct(self):
+        self.assertIsInstance(runSystemCommand('ipconfig'), bytes)
+
+    def testRunSystemCommand_badInput(self):
+        self.assertRaises(CalledProcessError, runSystemCommand, ['del'])        
+
+
+class testGetHostInfo(unittest.TestCase):
+
+    def testGetHostInfo_correct(self):
+        self.assertIsInstance(getHostInfo(), dict)
+        
+        
+class testDexterWriteLog(unittest.TestCase):
+
+    def testGetHostInfo_correct(self):
+        self.assertIsInstance(environ, dict)
+        self.assertEqual(writeLog('Dexter Test'), 0)
+        self.assertIsInstance(environ['DEXTERLOG'], str)
+        self.assertTrue(os.path.exists(environ['DEXTERLOG']))
+
+        
+if __name__ == '__main__':
+    unittest.main()
--- runSystemCommand.py
+++ runSystemCommand.py
@@ -7,7 +7,7 @@ import subprocess
 
 # Function expects an ordered list of the Windows command, incl. parameters
 def runSystemCommand(cmd):
-    return subprocess.check_output(cmd, shell=True)
+    return subprocess.check_output(cmd, shell=True, stderr=subprocess.DEVNULL)
 
 if __name__ == '__main__':
     del(argv[0])
